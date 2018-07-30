469e8a5400eb63791127367bb24ff037ae391250
Tim Hanson <timothy.hanson@kablab.com>
2018-07-30 12:06:04 -0700

    added vim airline

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ cd ~
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/morhetz/gruvbox.git ~/.vim/bundle/gruvbox
+git clone https://github.com/vim-airline/vim-airline.git ~/.vim/bundle/vim-airline
 echo "so ~/dotfiles/vimrc" >> ~/.vimrc
 echo "source-file ~/dotfiles/tmux.conf" >> ~/.tmux.conf
 echo "source ~/dotfiles/bashrc" >> ~/.bashrc
